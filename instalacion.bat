013c59024eb7782bf66950a6dd4fb40b9c8619cf
Kaleomy <jeanpaul.h.s87@gmail.com>
2025-10-03 22:45:10 -0500

    Instalación
    Ejecuta el cmd e instala las dependencias necesarias que en este caso es el flask y el pika.

---
+++ instalacion.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Instalando dependencias del proyecto UniDelivery...
+echo.
+
+pip install pika flask
+
+echo.
+echo Dependencias instaladas correctamente!
+echo Ahora puedes ejecutar el sistema con 'ejecutar_sistema.bat'
+pause
