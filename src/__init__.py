5c130de0e2cb996fb8eb69976af7d952cc36d1db
CesarAgain <cesarfonseca286@gmail.com>
2025-10-03 22:22:03 -0500

    Init
    Esquema en blanco

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+
+
+"""
+Paquete principal del sistema de pedidos UniDelivery
+Arquitectura basada en RabbitMQ - Patrón Publicador/Suscriptor
+"""
+
+__version__ = "1.0.0"
+__author__ = "Equipo UniDelivery"
+__description__ = "Sistema de gestión de pedidos para universidad usando RabbitMQ"
+__license__ = "MIT"
+
